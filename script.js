eeb81839d399d160a11e0af9dd790e3a7c0f6b1f
Hasitha Ekanayake <150508298+h4sitha@users.noreply.github.com>
2024-03-19 13:10:29 +0530

    Clean code

--- script.js
+++ script.js
@@ -3,11 +3,13 @@ const text = document.querySelector('#theme');
 const button = document.querySelector('button');
 
 button.addEventListener('click', () => {
+    let theme;
     if (!root.classList.contains("dark-mode")) {
-        text.textContent = "Dark";
+        theme = "Dark"
         root.classList.toggle("dark-mode");
     } else {
-        text.textContent = "Light";
+        theme = "Light";
         root.classList.toggle("dark-mode");
     }
+    text.textContent = theme;
 })
