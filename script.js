32f61c675f9dee79f454cbdf2458467be9bebb84
Hasitha Ekanayake <150508298+h4sitha@users.noreply.github.com>
2024-03-19 13:07:26 +0530

    Update button function to change theme

--- script.js
+++ script.js
@@ -3,9 +3,11 @@ const text = document.querySelector('#theme');
 const button = document.querySelector('button');
 
 button.addEventListener('click', () => {
-    if (text.textContent.toLowerCase() === "light") {
+    if (!root.classList.contains("dark-mode")) {
         text.textContent = "Dark";
+        root.classList.toggle("dark-mode");
     } else {
         text.textContent = "Light";
+        root.classList.toggle("dark-mode");
     }
 })
